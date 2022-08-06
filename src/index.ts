4a38112781f98df7a0e4d1a9b73b7b516fe5f75b
Katsuyuki Omuro <komuro@fastly.com>
2022-08-06 15:39:52 +0900

    Export more stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 /// <reference types="@fastly/js-compute" />
 
-export { toReqRes, toComputeResponse } from './http-compute-js/http-server';
+export { ComputeJsIncomingMessage } from './http-compute-js/http-incoming';
+export { ComputeJsOutgoingMessage } from './http-compute-js/http-outgoing';
+export { toReqRes, toComputeResponse, ReqRes, ToReqResOptions, ComputeJsServerResponse } from './http-compute-js/http-server';
+
 export * as polyfill from './polyfill';
